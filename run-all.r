cc1d61dda27dd18158912b7930af39189fd1e380
Rob Hanssen <rob.hanssen@outlook.com>
2024-10-07 08:50:09 -0400

    fixed regexp

--- run-all.r
+++ run-all.r
@@ -2,6 +2,6 @@ source("defaults.r")
 source("import.r")
 
 scripts <-
-    list.files(path = "./scripts/", pattern = "*.r$", full.names = TRUE)
+    list.files(path = "./scripts/", pattern = "\\.r$", full.names = TRUE)
 
 walk(scripts, source)
