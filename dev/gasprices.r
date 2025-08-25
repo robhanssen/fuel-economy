a657ba3697763232251a3269c4e8735c58aebfae
Rob Hanssen <rob.hanssen@outlook.com>
2025-08-25 13:51:13 -0400

    updated fuelups

--- dev/gasprices.r
+++ dev/gasprices.r
@@ -15,12 +15,13 @@ theme_set(
 
 load("Rdata/fuel.Rdata")
 
-inaug <- ymd(c(20090121, 20170121, 20210121))
+inaug <- ymd(c(20090121, 20170121, 20210121, 20250121))
 
 fuel %>%
     ggplot(aes(x = date, y = price)) +
     geom_point() + 
     geom_vline(xintercept = inaug, color = "gray70", size = 1) + 
+    geom_hline(yintercept = 2.56, color = "gray70", size = 1) +
     scale_y_continuous(
         labels = scales::label_dollar(),
         limits = c(0, NA)
@@ -43,3 +44,14 @@ fuel %>%
         min(price),
         max(price),
     )
+
+with(fuel, sum(miles)/sum(gallons))
+
+fuel |> 
+    split(fuel$car_name) |>
+    map_df(~with(., sum(miles)/sum(gallons))) %>%
+    pivot_longer(everything(), names_to = "car_name", values_to = "mpg")
+
+fuel %>%
+    filter(!str_detect(car_name, "2008")) %>%
+    summarize(mpg = sum(cost)/sum(gallons))        
